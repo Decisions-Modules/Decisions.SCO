1a7156a0a44b6672fb7ed349b0f3fada01c70270
brandon-decisions <76183624+brandon-decisions@users.noreply.github.com>
2020-12-17 16:28:30 -0500

    Delete buildninstall.bat

--- buildninstall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Building with MsBuild
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build.proj
-IF NOT errorlevel 0 GOTO :error
-copy ..\..\output\modules\Decisions.SCO.zip "c:\Program Files\Decisions\Decisions Services Manager\modules" /Y
-net stop "Service Host Manager Watcher"
-net stop "Service Host Manager"
-net start "Service Host Manager"
